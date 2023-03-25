6926b4c26e8b680465b30d9eae20b3b3ff8ca500
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-03-25 16:41:35 +0700

    feat: remove unused attrs

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -58,9 +58,6 @@ const bufferSetup = (contextGL: ContextGL) => {
   gl.useProgram(shaderProgram);
 
   gl.enable(gl.DEPTH_TEST);
-  gl.enable(gl.CULL_FACE);
-  // gl.enable(gl.CULL_FACE);
-  // console.log("cull face")
   gl.depthFunc(gl.LEQUAL);
 
   contextGL.clear();
